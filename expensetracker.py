53e8e6fe0e99f933c404e9d8bc855b7c4c8e335d
Arjun Kuzhikkattil Saji <arjunachusaji@gmail.com>
2026-07-07 18:24:55 +0300

    Add expense tracker project

---
+++ expensetracker.py
@@ -0,0 +1,39 @@
+
+expenses =[]
+while True:
+    print("--- Expense Tracker ---")
+    print("1. Add Expense")
+    print("2. View Expenses")
+    print("3. Exit")
+    print("4. Delete Expense")
+    inputnumber = int(input("Enter a number:"))
+    
+    if inputnumber == 1:
+        print("Add expense selected")
+        amount = int(input("Enter the amount:"))
+        category = input("Enter the category:")
+        myexpensedict = {"Amount": amount, "Category": category}
+        expenses.append(myexpensedict)
+    elif inputnumber ==2:
+        print("View expenses selected")
+        for index, item in enumerate(expenses):
+            print("Expense", index +1, "Amount = ", item["Amount"], "Catergory = ", item["Category"])
+
+
+        
+             
+    elif inputnumber ==3:
+         print("Goodbye")
+         break
+    elif inputnumber ==4:
+        deletenumberinput = int(input("Enter the number to be deleted: "))
+        realposition = deletenumberinput - 1
+        del expenses[realposition]
+        print("Expense deleted")
+
+        
+    else:
+        print("Invalid option, try again:")
+ 
+        
+
